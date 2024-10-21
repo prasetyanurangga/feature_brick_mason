39cf2f38b1599ba3820c818f98938eb1b0b54e1a
Angga Nur Prasetya <angganurprasetya4@gmail.com>
2024-10-21 10:03:30 +0700

    Update pre_gen.dart

--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -1,5 +1,12 @@
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  // TODO: add pre-generation logic.
+  final bool isGlobal = context.vars['is_global'] as bool;
+
+  if (isGlobal) {
+    context.vars['is_with_page'] = false;
+    context.vars['is_with_widget'] = false;
+    context.logger.info('Global mode enabled: skipping page and widget generation.');
+  }
+
 }
