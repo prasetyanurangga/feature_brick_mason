e57b710c8c168f4ed078fdcb0cb370a83395745e
Angga <angga.prasetya@traspac.co.id>
2024-10-21 10:21:07 +0700

    Change output Directory

--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -2,30 +2,8 @@ import 'package:mason/mason.dart';
 import 'dart:io';
 
 void run(HookContext context) {
-  final bool isGlobal = context.vars['is_global'] as bool;
+  final outputDir = Directory(context.vars['output_dir'] as String);
 
-  // Determine the output directory based on whether it's global
-  String outputDirectory;
-
-  if (isGlobal) {
-    context.vars['is_with_page'] = false;
-    context.vars['is_with_widget'] = false;
-    context.logger.info('Global mode enabled: skipping page and widget generation.');
-    outputDirectory = 'shared/global_features';
-  } else {
-    outputDirectory = 'features';
-  }
-
-  // Create the output directory if it doesn't exist
-  final outputDir = Directory(outputDirectory);
-  if (!outputDir.existsSync()) {
-    outputDir.createSync(recursive: true);
-  }
-
-  // Print info to the console
-  context.logger.info('Generating files in: $outputDirectory');
-
-  // Set the output directory for mason
-  context.vars['output_directory'] = outputDirectory;
+  context.logger.info('Output Directory: ${outputDir.path}');
 
 }
